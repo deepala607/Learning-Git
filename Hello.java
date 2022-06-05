6ec80f140d003e76e705eba2182a1da84155ca68
bhanu <bhanu.105607@gmail.com>
2022-06-05 16:39:15 -0400

    additional sample commandline cases

--- Hello.java
+++ Hello.java
@@ -1,6 +1,12 @@
-Class Hello{
+package com.learning;
+
+class Hello{
 
 public static void main(String[] args){
 System.out.println("hello git!!!");
+System.out.println("Command to execute me as I have com.learning package: ");
+System.out.println("javac -d . Hello.hava ");
+System.out.println("java com.learning.Hello ");
+
 }
 }
